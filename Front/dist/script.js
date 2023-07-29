d0651e494ad08a1544a966d18176edbc2d5c892b
elzondao <ndaoelhadji973@gmail.com>
2023-07-29 14:13:39 +0000

    formulaire pour les transactions

--- Front/dist/script.js
+++ Front/dist/script.js
@@ -1,1 +1,21 @@
-// console.log('hey');
+var _a;
+(_a = document.getElementById('fournisseur')) === null || _a === void 0 ? void 0 : _a.addEventListener('change', function () {
+    const selectElement = document.getElementById('fournisseur');
+    const transactionTitles = document.querySelectorAll('.transaction-title');
+    const selectedFournisseur = selectElement.value;
+    transactionTitles.forEach(transactionTitle => {
+        transactionTitle.classList.remove('om', 'wv', 'wr', 'cb');
+        if (selectedFournisseur === 'om') {
+            transactionTitle.classList.add('om');
+        }
+        else if (selectedFournisseur === 'wv') {
+            transactionTitle.classList.add('wv');
+        }
+        else if (selectedFournisseur === 'wr') {
+            transactionTitle.classList.add('wr');
+        }
+        else if (selectedFournisseur === 'cb') {
+            transactionTitle.classList.add('cb');
+        }
+    });
+});
--- Front/src/script.ts
+++ Front/src/script.ts
@@ -1,1 +1,21 @@
-// console.log('hey');
+ 
+ document.getElementById('fournisseur')?.addEventListener('change', function () {
+    const selectElement = document.getElementById('fournisseur') as HTMLSelectElement;
+    const transactionTitles = document.querySelectorAll('.transaction-title') as NodeListOf<HTMLHeadingElement>;
+    const selectedFournisseur = selectElement.value;
+
+    transactionTitles.forEach(transactionTitle => {
+
+        transactionTitle.classList.remove('om', 'wv', 'wr', 'cb');
+
+        if (selectedFournisseur === 'om') {
+            transactionTitle.classList.add('om');
+        } else if (selectedFournisseur === 'wv') {
+            transactionTitle.classList.add('wv');
+        } else if (selectedFournisseur === 'wr') {
+            transactionTitle.classList.add('wr');
+        } else if (selectedFournisseur === 'cb') {
+            transactionTitle.classList.add('cb');
+        }
+    })
+});
